2675b66209c802356a799b1ae8082dddac3fc508
prasangafdz@outlook.com <LykanHypersp0rt@#>
2018-11-17 16:57:06 +0530

    Added some Xpaths

--- src/seleniumtest/Main.java
+++ src/seleniumtest/Main.java
@@ -1,18 +1,29 @@
 package seleniumtest;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Created by Prasanga Fernando
 
 		WebDriver driver = new ChromeDriver();
 		
 		System.setProperty("webdriver.chrome.driver","/seleniumtest/selenium/chromedriver.exe");
 
-		driver.get("https://www.seleniumhq.org/download/");
+		//driver.get("https://www.seleniumhq.org/download/");
+		driver.get("file:///C:/Users/Prasanga%20Fernando/Documents/Development/Web/Bootstrap/form.html?");
+		
+		//driver.manage().window().maximize();//To maximize the window.
+		
+		driver.findElement(By.xpath("//*[@id=\"exampleInputEmail1\"]")).sendKeys("fprasanga@virtusa.com");
+		
+		driver.findElement(By.xpath("//*[@id=\"btn-submit\"]")).click();
+		
+		
+		////*[@id="exampleInputEmail1"]
 		
 	}
 
